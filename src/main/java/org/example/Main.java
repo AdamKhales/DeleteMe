5cac28ccbdf0a0f86fcc978891727d68fb8e1b59
Adam Khales <2493077@edu.vaniercollege.qc.ca>
2025-04-29 15:22:06 -0400

    Add insertionSort in Main and modified selectionSort in Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -32,9 +32,9 @@ public class Main {
     11234 5
      */
     public static void selectionSort(int[] nums) {
-        for (int i = 0; i < nums.length - 1; i++) {
+        for (int i = 0; i < nums.length; i++) {
             int minIdx = i;
-            for (int j = i + 1; j < nums.length - 1; j++) {
+            for (int j = i; j < nums.length; j++) {
                 if (nums[j] < nums[minIdx]) {
                     minIdx = j;
                 }
@@ -42,6 +42,25 @@ public class Main {
             int temp = nums[i];
             nums[i] = nums[minIdx];
             nums[minIdx] = temp;
+        }
+    }
+    /*
+    3     15214
+    1     35214
+    13    5214
+    123   514
+    1123  54
+    11234 5
+     */
+    public static void insertionSort(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            int key = nums[i];
+            int j = i - 1;
+            while (j >= 0 && nums[j] > key) {
+                nums[j + 1] = nums[j];
+                j--;
+                nums[j + 1] = key;
+            }
         }
     }
 }
